2f13fa00e3a16913d4fe12cadf148bb7e828a878
genffy <genffyl@gmail.com>
2022-06-15 00:46:37 +0800

    feat: add code highlight

--- components/blocks/quote.tsx
+++ components/blocks/quote.tsx
@@ -1,6 +1,12 @@
-const Quote = ({ data }: any) => {
+import blocksStyles from '@/styles/blocks.module.css';
 
-    return null;
-  };
-  
-  export default Quote;
+const Quote = ({ data }: any) => {
+  console.log('data', data)
+  return <div>
+    <blockquote className={blocksStyles.blockquote}>
+      <p><em>There’s no denying a WhatsApp CRM Integration provides many benefits for growing and established businesses. But first, there are certain things you need to understand about these integrations and sort out.</em></p>
+    </blockquote>
+  </div>;
+};
+
+export default Quote;
