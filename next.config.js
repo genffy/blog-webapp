a33880d2cbcfc992ba74dfa1333f69134ed20f84
genffy <genffyl@gmail.com>
2022-10-12 00:33:54 +0800

    feat: updrage

--- next.config.js
+++ next.config.js
@@ -2,18 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     loader: "default",
     // OSS 中域名的地址
     domains: ["blog-genffy.oss-cn-hangzhou.aliyuncs.com"],
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return defaultPathMap;
+  },
 }
 
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-module.exports = (phase, { defaultConfig }) =>{
-  if(phase === PHASE_DEVELOPMENT_SERVER){
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
     nextConfig.images.domains.push('localhost');
   }
   return {
     ...nextConfig
-  } 
+  }
 }
